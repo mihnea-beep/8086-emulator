160a300d8e65cce3d3cc0aead5484167b73a35ab
mihnea-beep <mihneadragodanescu@yahoo.com>
2021-06-02 14:51:54 +0300

    CPU, Assembler and GUI in the same class

--- Scene.h
+++ Scene.h
@@ -5,18 +5,23 @@
 #include <vector>
 #include "GUI.h"
 #include "Button.h"
+#include "emu/Assembler.h"
+#include "emu/CPU.h"
 using namespace std;
 
 class Scene
 {
   bool scene_running = false;
-  
+
   GUI element;
   Button msg[4]; // 4 buttons
   bool assemblePressed = false;
   bool exitPressed = false;
   bool editPressed = false;
   bool executePressed = false;
+  Assembler assembler;
+  CPU cpu;
+
 
 public:
   Scene();
@@ -35,6 +40,8 @@ public:
   virtual void free();
   static void editFile();
   static void launchCPU();
+
+  virtual void startCPU();
 
   virtual ~Scene();
 };
